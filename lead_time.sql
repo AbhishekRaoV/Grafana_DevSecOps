7b3ced0ddb6bc1afbfb681db297cbf49904fc50a
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-02-26 10:22:20 +0000

    Update lead_time.sql

--- lead_time.sql
+++ lead_time.sql
@@ -2,20 +2,20 @@ SELECT
     CONCAT(
         IF(
             AVG(TIMESTAMPDIFF(DAY, created_date, updated_date)) > 0,
-            CONCAT(ROUND(AVG(TIMESTAMPDIFF(DAY, created_date, updated_date))), 'days'),
-            '0days'
+            CONCAT(ROUND(AVG(TIMESTAMPDIFF(DAY, created_date, updated_date))), 'Days'),
+            '0 Days '
         ),
         IF(
             AVG(TIME_FORMAT(TIMEDIFF(updated_date, created_date), '%H')) > 0,
-            CONCAT(ROUND(AVG(TIME_FORMAT(TIMEDIFF(updated_date, created_date), '%H'))), 'hours'),
-            '0hours'
+            CONCAT(ROUND(AVG(TIME_FORMAT(TIMEDIFF(updated_date, created_date), '%H'))), 'Hours'),
+            '0 Hours '
         ),
         IF(
             AVG(TIME_FORMAT(TIMEDIFF(updated_date, created_date), '%i')) > 0,
-            CONCAT(ROUND(AVG(TIME_FORMAT(TIMEDIFF(updated_date, created_date), '%i'))), 'mins'),
-            '0mins'
+            CONCAT(ROUND(AVG(TIME_FORMAT(TIMEDIFF(updated_date, created_date), '%i'))), ' Mins'),
+            '0 Mins'
         )
-    ) AS avg_days_and_hours_difference
+    ) AS 'Sprint 11'
 FROM
     issues
 WHERE
